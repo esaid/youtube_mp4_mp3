ab5147bbb89ca1483614bab33c20971d64bcbb8e
Ga144 <emmanuel.said@gmail.com>
2020-02-26 10:33:37 +0100

    gestion fichier
    amelioration de la gestion pour renommer un fichier,
    suppression des caracteres non supportes par windows
    taille nom de fichier a 100 caracteres

--- youtube.py
+++ youtube.py
@@ -5,20 +5,26 @@ import subprocess
 import time
 import datetime
 
+liste_charactere = ['/', ':', '\\', '*', '?', '<', '>', '|', '.', '-','"',"'"," "]  # caracteres a supprimer
+def delete_character (s):
+    for i in liste_charactere:
+       s = s.replace(i,'')
+
+    return s
+
+
 # generation d'un string avec date et temps
 date = str(datetime.datetime.now())
-date = date.replace('.', '')
-date = date.replace(' ', '')
-date = date.replace(':', '')
-date = date.replace('-', '')
+date = delete_character(date)
 _filename = 'file' + date
 
 mp4 = "%s.mp4" % _filename
 mp3 = "%s.mp3" % _filename
 url = input("URL: ")
 # url = "https://www.youtube.com/watch?v=ITnLNoWOwFU"  exemple
-# filename_rename : prend les 60 premiers caracteres et remplace les espaces par _
-_filename_rename = YouTube(url).title.replace(' ', "_")[:70]
+# filename_rename : prend les 100 premiers caracteres et remplace les espaces par _
+_filename_rename = YouTube(url).title.replace(' ', "_")[:100]
+_filename_rename = delete_character(_filename_rename)
 print(_filename_rename)
 # Download
 print("\nDownloading....\n")
@@ -30,10 +36,17 @@ ffmpeg_param = 'ffmpeg -i %s -vn %s' % (mp4, mp3)
 # print (ffmpeg_param)
 ffmpeg = ffmpeg_param
 subprocess.call(ffmpeg, shell=True)
+
+time.sleep(1)
+try:
+    os.rename(mp4, _filename_rename + ".mp4")
+except:
+    print("je n'arrive pas a renommer le fichier mp4")
+time.sleep(1)
+try:
+    os.rename(mp3, _filename_rename + ".mp3")
+except:
+    print("je n'arrive pas a renommer le fichier mp3")
+time.sleep(1)
 # end
 print("\nEnd\n")
-time.sleep(1)
-os.rename(mp4, _filename_rename + ".mp4")
-time.sleep(1)
-os.rename(mp3, _filename_rename + ".mp3")
-time.sleep(1)
